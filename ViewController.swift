636b48d89cbebc89b56c4d6761b1f4a212f99205
plamistef <plamstef@icloud.com>
2020-03-20 12:28:33 +0200

    init project

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapDemo
+//
+//  Created by Plam Stefanova on 3/20/20.
+//  Copyright © 2020 Plam Stefanova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
+
